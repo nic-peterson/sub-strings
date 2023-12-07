fa8919d52aa204d12f671b231b59bec610e891b6
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-12-06 16:19:36 -0800

    Finished assignment.

--- substring.rb
+++ substring.rb
@@ -1,1 +1,13 @@
-puts "Hello World!"
+def substrings(word, dictionary)
+  result = {}
+  dictionary.each do |substring|
+    if word.downcase.include?(substring)
+      result[substring] = word.downcase.scan(substring).length
+    end
+  end
+  result
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
