474b458a311aa02a455b4efb8f3b987b285e79c3
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-12-06 16:20:58 -0800

    Removed testing examples.

--- substring.rb
+++ substring.rb
@@ -8,6 +8,3 @@ def substrings(word, dictionary)
   result
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings("below", dictionary)
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
